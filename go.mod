be4c99873ac36373e1c34727e3d3cc5314dc7ae9
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2020-05-20 09:21:34 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module yapperbot-uncurrenter
+
+go 1.14
+
+require (
+	cgt.name/pkg/go-mwclient v1.0.3
+	github.com/mashedkeyboard/ybtools v0.1.4
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+//
+// Uncurrenter, the {{current}} tag removal bot for Wikipedia
+// Copyright (C) 2020 Naypta
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import (
+	"log"
+	"os"
+	"regexp"
+	"time"
+
+	"cgt.name/pkg/go-mwclient"
+	"cgt.name/pkg/go-mwclient/params"
+	"github.com/mashedkeyboard/ybtools"
+)
+
+const botName string = "Yapperbot"
+
+var currentTemplateRegex *regexp.Regexp
+
+func main() {
+	var w *mwclient.Client
+	var err error
+
+	ybtools.SetupBot(botName)
+	currentTemplateRegex = regexp.MustCompile(`(?i){{current(?:{{[^}{]*}}|[^}{]*)*}}\n?`)
+
+	w, err = mwclient.New(os.Getenv("WP_BOT_ENDPOINT"), "Uncurrenter v1")
+	if err != nil {
+		log.Fatal("Failed to create MediaWiki client with error ", err)
+	}
+
+	err = w.Login(os.Getenv("WP_BOT_USERNAME"), os.Getenv("WP_BOT_PASSWORD"))
+	if err != nil {
+		log.Fatal("Failed to authenticate with MediaWiki with error ", err)
+	}
+
+	parameters := params.Values{
+		"action":         "query",
+		"prop":           "revisions",
+		"generator":      "embeddedin",
+		"geititle":       "Template:Current",
+		"geinamespace":   "0",
+		"geifilterredir": "nonredirects",
+		"rvprop":         "timestamp|content",
+		"rvslots":        "main",
+	}
+
+	query := w.NewQuery(parameters)
+	for query.Next() {
+		pages := ybtools.GetPagesFromQuery(query.Resp())
+		if len(pages) > 0 {
+			for _, page := range pages {
+				pageTitle, err := page.GetString("title")
+				if err != nil {
+					log.Println("Failed to get title from page, so skipping it. Error was", err)
+					continue
+				}
+
+				pageContent, err := ybtools.GetContentFromPage(page)
+				if err != nil {
+					log.Println("Failed to get content from page", pageTitle, ", so skipping it. Error was", err)
+					continue
+				}
+
+				pageRevisions, err := page.GetObjectArray("revisions")
+				if err != nil {
+					log.Println("Failed to get revisions array from page, so skipping it. Error was", err)
+					continue
+				}
+				lastTimestamp, err := pageRevisions[0].GetString("timestamp")
+				if err != nil {
+					log.Println("Failed to get timestamp from revision, so skipping the page. Error was", err)
+					continue
+				}
+				lastTimestampProcessed, err := time.Parse(time.RFC3339, lastTimestamp)
+				if err != nil {
+					log.Println("Failed to parse last revision timestamp, so skipping the page. Error was", err)
+					continue
+				}
+
+				// if it's been more than five hours since the last edit, and we can edit it
+				if time.Now().Sub(lastTimestampProcessed).Hours() > 5 && ybtools.BotAllowed(pageContent) {
+					newPageContent := currentTemplateRegex.ReplaceAllString(pageContent, "")
+					err = w.Edit(params.Values{
+						"title":    pageTitle,
+						"text":     newPageContent,
+						"summary":  "Removing the {{current}} template as the article hasn't been edited in over five hours. Did I get this wrong? Please revert me and check my userpage!",
+						"notminor": "true",
+						"bot":      "true",
+					})
+					if err == nil {
+						log.Println("Successfully removed current template from", pageTitle)
+					} else {
+						switch err.(type) {
+						case mwclient.APIError:
+							if err.(mwclient.APIError).Code == "editconflict" {
+								log.Println("Edit conflicted on page", pageTitle, "assuming it's still active and skipping")
+								continue
+							} else {
+								log.Fatal("API error raised, can't handle, so failing. Error was ", err)
+							}
+						default:
+							log.Fatal("Non-API error raised, can't handle, so failing. Error was ", err)
+						}
+					}
+				}
+			}
+		}
+	}
+}
